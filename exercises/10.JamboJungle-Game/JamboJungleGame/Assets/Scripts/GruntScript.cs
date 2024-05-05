0b595ea4e5ba857c2a0dc09f1a6d3c17f3017846
Luis Arrieta <luisarrieta796@gmail.com>
2024-05-04 19:26:07 -0500

    Hacer que el enemigo dispare si el personaje se encuentra a cierta distancia

--- exercises/10.JamboJungle-Game/JamboJungleGame/Assets/Scripts/GruntScript.cs
+++ exercises/10.JamboJungle-Game/JamboJungleGame/Assets/Scripts/GruntScript.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class GruntScript : MonoBehaviour
 {
+    public GameObject BulletPrefab;
     public GameObject John;
+
+    private float LastShoot;
 
     void Update()
     {
@@ -13,5 +16,21 @@ public class GruntScript : MonoBehaviour
         if (direction.x >= 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         else transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
 
+        float distance = Mathf.Abs(John.transform.position.x - transform.position.x);
+        if (distance < 1.0f && Time.time > LastShoot + 0.25f) 
+        { 
+            Shoot();
+            LastShoot = Time.time;
+        }
+    }
+
+    private void Shoot()
+    {
+        Vector3 direction;
+        if (transform.localScale.x == 1.0f) direction = Vector3.right;
+        else direction = Vector3.left;
+
+        GameObject bullet = Instantiate(BulletPrefab, transform.position + direction * 0.1f, Quaternion.identity);
+        bullet.GetComponent<BulletScript>().SetDirection(direction);
     }
 }
